41160d8f9649d6db0b09f1bd55147877b1de137c
Aminul Haque Rana <aminuhaquerana1@gmail.com>
2024-10-28 22:45:56 +0600

    avarage price logic

--- avarigePrice.js
+++ avarigePrice.js
@@ -1,12 +1,3 @@
-// Task-3:
-// Your task is to calculate the total budget required to buy electronics:
-
-//     laptop = 35000 tk
-//     tablet = 15000 tk
-//     mobile = 20000 tk
-// Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
-//
-
 const phones = [
   { model: "PhoneA", brand: "Iphone", price: 95000 },
   { model: "PhoneB", brand: "Samsung", price: 40000 },
